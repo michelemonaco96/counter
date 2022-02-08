353700a75480bbca464e4caec29959d9037acd77
mikytr96 <98319500+mikytr96@users.noreply.github.com>
2022-02-08 16:00:10 +0100

    first commit

---
+++ javascript/javascript.js
@@ -0,0 +1,38 @@
+
+     let counterResult = document.querySelector('.counter-result');
+     let counterMinusOp = document.querySelector('.counter-minus');
+     let counterPlusOp = document.querySelector('.counter-plus');
+     let resetResult = document.querySelector('.reset-button');
+     let count = 0;
+
+     showResult();
+
+     counterPlusOp.addEventListener("click",()=>{
+        count++;
+        showResult();
+      });
+
+      counterMinusOp.addEventListener("click",()=>{
+        count--;
+        showResult();
+      });
+
+      resetResult.addEventListener("click", ()=>{
+        if (confirm("Are you sure you want to delete the current count?")) {
+          count = 0;
+          showResult();
+        }
+        else {
+          return count;  
+        }
+               
+        }); 
+      
+      function showResult() {
+        counterResult.innerHTML = count;
+      };
+
+    
+
+
+
