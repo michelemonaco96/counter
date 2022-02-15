a86734bb621b9f8cbcef840e72cc3bac063614ed
mikytr96 <98319500+mikytr96@users.noreply.github.com>
2022-02-15 10:17:29 +0100

    "formatting"

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,38 +1,30 @@
+let counterResult = document.querySelector(".counter-result");
+let counterMinusOp = document.querySelector(".counter-minus");
+let counterPlusOp = document.querySelector(".counter-plus");
+let resetResult = document.querySelector(".reset-button");
+let count = 0;
 
-     let counterResult = document.querySelector('.counter-result');
-     let counterMinusOp = document.querySelector('.counter-minus');
-     let counterPlusOp = document.querySelector('.counter-plus');
-     let resetResult = document.querySelector('.reset-button');
-     let count = 0;
+showResult();
 
-     showResult();
+counterPlusOp.addEventListener("click", () => {
+  count++;
+  showResult();
+});
 
-     counterPlusOp.addEventListener("click",()=>{
-        count++;
-        showResult();
-      });
+counterMinusOp.addEventListener("click", () => {
+  count--;
+  showResult();
+});
 
-      counterMinusOp.addEventListener("click",()=>{
-        count--;
-        showResult();
-      });
+resetResult.addEventListener("click", () => {
+  if (confirm("Are you sure you want to delete the current count?")) {
+    count = 0;
+    showResult();
+  } else {
+    return count;
+  }
+});
 
-      resetResult.addEventListener("click", ()=>{
-        if (confirm("Are you sure you want to delete the current count?")) {
-          count = 0;
-          showResult();
-        }
-        else {
-          return count;  
-        }
-               
-        }); 
-      
-      function showResult() {
-        counterResult.innerHTML = count;
-      };
-
-    
-
-
-
+function showResult() {
+  counterResult.innerHTML = count;
+}
